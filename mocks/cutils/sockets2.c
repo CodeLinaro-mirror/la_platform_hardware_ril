5285abc716432cb6191e92e72332e0c55a4373d4
Yujing Gu <quic_yujingg@quicinc.com>
2022-01-07 17:47:59 +0800

    Add qsocket client and server API.
    1.Add qsocket server API to create a qrtr socket server
    with the specific service and instance ID.
    2.Add qsocket client API to create a socket client and
    lookup a qrtr socket server with the specific service
    and instance ID.
    
    Change-Id: I766952255b828a60c27a8947b3fefe62daab5c8a

--- mocks/cutils/sockets2.c
+++ mocks/cutils/sockets2.c
@@ -27,6 +27,42 @@
  *   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ *  Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ *  Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted (subject to the limitations in the
+ *  disclaimer below) provided that the following conditions are met:
+ *
+ *      * Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *      * Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials provided
+ *        with the distribution.
+ *
+ *      * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *        contributors may be used to endorse or promote products derived
+ *        from this software without specific prior written permission.
+ *
+ *  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ *  GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ *  HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ *  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "sockets2.h"
 #include <sys/socket.h>
 #include <sys/un.h>
@@ -34,10 +70,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <glib.h>
+#include <syslog.h>
+#include <errno.h>
+#include <libqrtr.h>
 
 #ifndef SOCKET_PATH_PREFIX
 #define SOCKET_PATH_PREFIX "/run/"
 #endif
+#define DONT_CARE_PORT_NUM 0
 
 static int make_sockaddr_un(const char *name, int type, struct sockaddr_un* addr) {
     int namelen = strlen(name) + strlen(SOCKET_PATH_PREFIX);
@@ -95,4 +135,54 @@ int ril_socket_local_client(const char *name, int type) {
     }
 
     return s;
+}
+
+/*
+ * Create a qrtr socket server with the specific service and instance ID.
+ *
+ * @param[in] service - service ID to set to the qrtr socket server
+ * @param[in] instance - instance ID to set to the qrtr the qsocket server
+ */
+int get_qsocket_server(const int service, const int instance) {
+    int fd = qrtr_open(DONT_CARE_PORT_NUM);
+    if (fd < 0) {
+        syslog(LOG_ERR, "%s:[%d] qrtr_open failed %d for service = %d, instance = %d",
+            __func__, __LINE__, fd, service, instance);
+        return SOCKET_GENERIC_FAILURE;
+    }
+    int rc = qrtr_publish(fd, service, 0, instance);
+    if (rc < 0) {
+        syslog(LOG_ERR, "%s:[%d] publish failed: %s, rc = %d", __func__, __LINE__,
+            strerror(errno), rc);
+        qrtr_close(fd);
+        return PUBLISH_FAILURE;
+    }
+
+    return fd;
+}
+
+/*
+ * Create a socket client and lookup a qrtr socket server with the specific
+ * service and instance ID.
+ *
+ * @param[in]  service - service ID to query the qrtr socket server
+ * @param[in]  instance - instance ID to query the qrtr socket server
+ */
+int get_qsocket_client(const int service, const int instance) {
+    int fd = qrtr_open(DONT_CARE_PORT_NUM);
+    if (fd < 0) {
+        syslog(LOG_ERR, "%s:[%d] qrtr_open failed %d for service = %d, instance = %d",
+            __func__, __LINE__, fd, service, instance);
+        return SOCKET_GENERIC_FAILURE;
+    }
+
+    int rc = qrtr_new_lookup(fd, service, 0, instance);
+    if (rc < 0) {
+        syslog(LOG_ERR, "%s:[%d] lookup failed: %s, rc = %d", __func__, __LINE__,
+            strerror(errno), rc);
+        qrtr_close(fd);
+        return LOOKUP_FAILURE;
+    }
+
+    return fd;
 }
--- mocks/cutils/sockets2.h
+++ mocks/cutils/sockets2.h
@@ -27,10 +27,51 @@
  *   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ *  Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ *  Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted (subject to the limitations in the
+ *  disclaimer below) provided that the following conditions are met:
+ *
+ *      * Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *      * Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials provided
+ *        with the distribution.
+ *
+ *      * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *        contributors may be used to endorse or promote products derived
+ *        from this software without specific prior written permission.
+ *
+ *  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ *  GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ *  HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ *  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef SOURCES_SOCKETS_H
 #define SOURCES_SOCKETS_H
 
+#define SOCKET_GENERIC_FAILURE -1
+#define PUBLISH_FAILURE -2
+#define LOOKUP_FAILURE -3
+
 int ril_socket_local_server(const char *name, int type);
 int ril_socket_local_client(const char *name, int type);
-
+int get_qsocket_server(const int service, const int instance);
+int get_qsocket_client(const int service, const int instance);
 #endif //SOURCES_SOCKETS_H
