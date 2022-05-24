a8e9539fa148f2dedeccebe6c85db11cee290b89
Rama Lakshmi Peketi <quic_peketi@quicinc.com>
2022-05-24 14:59:25 +0530

    telux-service-ril: Invalid input for phone number leading to range error exception
    Issue: When some random text with special characters is entered
    instead of a valid number as an input to phone number, make call
    sample app is crashing with range_error exception.This is during
    the UTF8 to UTF16 conversion before sending to RIL
    
    Fix: Add a try catch block to catch the exception during the
    conversion and return the error.
    
    CRs-Fixed: 3129146
    
    Change-Id: I4fd9d6dba146a4f57df0d86f432bcd9875defc51

--- mocks/binder/Parcel2.cpp
+++ mocks/binder/Parcel2.cpp
@@ -19,6 +19,42 @@
  * limitations under the License.
  */
 
+/*
+ *  Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ *  Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <binder/Parcel2.h>
 #include <utils/Errors2.h>
 #include <cstring>
@@ -525,12 +561,19 @@ status_t Parcel::writeString8AsString16(const char *s8) {
     std::string source(s8);
     std::u16string u16_conv;
 
-    if (isLittleEndian) {
-        u16_conv = std::wstring_convert<std::codecvt_utf8_utf16<char16_t, 0x10ffff,
-                std::little_endian>, char16_t>{}.from_bytes(source);
-    } else {
-        u16_conv = std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t>{}
-                .from_bytes(source);
+    //In case the input is provided as string instead of UTF8 charset which includes any special
+    // characters, then a range error excpetion is thrown.
+    //Try-catch block is added to handle the error exception and return as invalid value.
+    try {
+       if (isLittleEndian) {
+           u16_conv = std::wstring_convert<std::codecvt_utf8_utf16<char16_t, 0x10ffff,
+               std::little_endian>, char16_t>{}.from_bytes(source);
+       } else {
+           u16_conv = std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t>{}
+               .from_bytes(source);
+       }
+    } catch(const std::range_error & exception) {
+        return BAD_VALUE;
     }
 
     int len = u16_conv.length();
