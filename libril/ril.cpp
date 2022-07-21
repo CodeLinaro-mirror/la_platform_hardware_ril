93e8d1462358a1a83cf28fdd683942163252d021
Siva Kumar Mellemputi <quic_smellemp@quicinc.com>
2022-07-21 14:20:22 +0530

    ril: Compile time optimizations in rild
    - Disable rild-debug socket and SAP socket as they are unsed in Telematics
    - Disable emulator related logic
    
    CRs-Fixed: 3309723
    
    Change-Id: I9917ea5dfff6090222f1c9230955a5980f5127ae

--- libril/ril.cpp
+++ libril/ril.cpp
@@ -4740,6 +4740,7 @@ static void listenCallback (int fd, short flags, void *param) {
     }
 }
 
+#ifndef RIL_FOR_MDM_LE
 static void freeDebugCallbackArgs(int number, char **args) {
     for (int i = 0; i < number; i++) {
         if (args[i] != NULL) {
@@ -4942,6 +4943,7 @@ static void debugCallback (int fd, short flags, void *param) {
     freeDebugCallbackArgs(number, args);
     close(acceptFD);
 }
+#endif // RIL_FOR_MDM_LE
 
 
 static void userTimerCallback (int fd, short flags, void *param) {
@@ -5216,7 +5218,7 @@ RIL_register (const RIL_RadioFunctions *callbacks) {
 #endif /* (SIM_COUNT == 4) */
 
 
-#if 1
+#ifndef RIL_FOR_MDM_LE
     // start debug interface socket
 
     char *inst = NULL;
@@ -5228,14 +5230,9 @@ RIL_register (const RIL_RadioFunctions *callbacks) {
     if (inst != NULL) {
         strlcat(rildebug, inst, MAX_DEBUG_SOCKET_NAME_LENGTH);
     }
-
-#ifndef RIL_FOR_MDM_LE
     s_fdDebug = android_get_control_socket(rildebug);
-#endif
     if (s_fdDebug < 0) {
-#ifndef RIL_FOR_MDM_LE
         RLOGW("Failed to get socket : %s errno:%d, creating local socket", rildebug, errno);
-#endif
         s_fdDebug = ril_socket_local_server(rildebug, SOCK_STREAM);
         if (s_fdDebug < 0) {
             RLOGW("Failed to create socket : %s errno:%d", rildebug, errno);
@@ -5255,7 +5252,7 @@ RIL_register (const RIL_RadioFunctions *callbacks) {
                 debugCallback, NULL);
 
     rilEventAddWakeup (&s_debug_event);
-#endif
+#endif // RIL_FOR_MDM_LE
 
 }
 
