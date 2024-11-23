5780a65b94a861a9e45382cdfcdddb187f8e1632
Yujing Gu <quic_yujingg@quicinc.com>
2024-11-23 12:52:49 +0800

    ril: Add new RIL requests to set and get IMS VoNR service status
    Add new RIL requests to set and get IMS VoNR service status
    
    CRs-Fixed: 3982631
    Change-Id: I5851f7dd0e93488b899427b865bf95af5f581d49

--- libril/ril_commands.h
+++ libril/ril_commands.h
@@ -174,4 +174,6 @@
     {RIL_REQUEST_MODIFY_CALL_INITIATE, dispatchModify, responseVoid},
     {RIL_REQUEST_MODIFY_CALL_CONFIRM, dispatchModify, responseVoid},
     {RIL_REQUEST_RESTART_ECALL_HLAP_TIMER, dispatchRestartEcallHlapTimer, responseVoid},
+    {RIL_REQUEST_SET_IMS_VONR, dispatchInts, responseVoid},
+    {RIL_REQUEST_GET_IMS_VONR, dispatchVoid, responseInts}
 #endif  /* RIL_FOR_MDM_LE */
