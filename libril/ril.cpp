f7d560c9efd66667f777687f1e1df86ede2962c3
Yujing Gu <quic_yujingg@quicinc.com>
2025-08-08 15:42:12 +0800

    ril: Add call mode to RIL call
    Add call mode to RIL call
    
    CRs-Fixed: 4261614
    Change-Id: I84587918492972776159aa10bf81b3d00e467174

--- libril/ril.cpp
+++ libril/ril.cpp
@@ -2876,6 +2876,7 @@ static void decodeCalls(Parcel &p, RIL_Call *p_cur) {
     p.writeInt32(p_cur->numberPresentation);
     p.writeString8AsString16(p_cur->name);
     p.writeInt32(p_cur->namePresentation);
+    p.writeInt32(p_cur->mode);
     // Remove when partners upgrade to version 3
     if ((s_callbacks.version < 3) || (p_cur->uusInfo == NULL || p_cur->uusInfo->uusData == NULL)) {
         p.writeInt32(0); /* UUS Information is absent */
@@ -2902,6 +2903,8 @@ static void decodeCalls(Parcel &p, RIL_Call *p_cur) {
         p_cur->numberPresentation,
         p_cur->name,
         p_cur->namePresentation);
+    RLOGD("callMode=%d]",
+        p_cur->mode);
 }
 
 static int responseSMS(Parcel &p, void *response, size_t responselen) {
